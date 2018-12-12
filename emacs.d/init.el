b32590b17d6f9ca0c5dd130f6f5b6a9e7eb83b1a
Daichi Mukai <mukai.daichi.37e@kyoto-u.jp>
2018-12-12 14:18:41 +0900

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,6 +21,36 @@
 
 (straight-use-package 'use-package)
 
+(defun my/open-init-el ()
+  "open init.el"
+  (interactive)
+  (find-file (expand-file-name "~/.emacs.d/init.el")))
+
+;;; leader key
+(define-prefix-command 'my-leader-map) ;; my leader key
+(bind-key "SPC" 'execute-extended-command my-leader-map)
+
+;;; file map
+(define-prefix-command 'my-file-map)
+(bind-keys :map my-leader-map
+	   :prefix "f" ;; bind to '<leader>f'
+	   :prefix-map my-file-map
+	   ("f" . find-file))
+
+;;; buffer map
+(define-prefix-command 'my-buffer-map)
+(bind-keys :map my-leader-map
+	   :prefix "b" ;; bind to '<leader>b'
+	   :prefix-map my-buffer-map
+	   ("b" . switch-to-buffer))
+
+;;; init.el map
+(define-prefix-command 'my-init-el-map)
+(bind-keys :map my-file-map
+	   :prefix "e" ;; bind to '<leader>fe'
+	   :prefix-map my-init-el-map
+	   ("d" . my/open-init-el))
+
 (use-package which-key
   :config (which-key-mode)
   :straight t)
@@ -31,12 +61,7 @@
 
 (use-package evil
   :config
-  (define-prefix-command 'my-leader-map) ;; my leader key
   (define-key evil-normal-state-map (kbd "SPC") 'my-leader-map) ;; change the leader key to space
-  (define-key my-leader-map "fed" (lambda () (interactive) (find-file (expand-file-name "~/.emacs.d/init.el"))))
-  (define-key my-leader-map "ff" 'find-file)
-  (define-key my-leader-map (kbd "SPC") 'execute-extended-command)
-  (define-key my-leader-map "bb" 'switch-to-buffer)
   (evil-mode)
   :straight t)
 
@@ -46,6 +71,17 @@
   :config (evil-escape-mode)
   :straight t)
 
+(use-package evil-magit :straight t
+  :after (evil magit)
+  :init (setq evil-magit-state 'normal))
+
 (use-package moe-theme
   :config (moe-dark)
   :straight t)
+
+;;; Completion
+(use-package ivy :straight t
+  :config (ivy-mode 1))
+
+(use-package swiper :straight t
+  :bind (("C-s" . swiper)))
